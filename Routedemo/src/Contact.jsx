d83dd1a681163c52d657466b8ca8ef6ee477492d
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-02-06 21:23:23 +0530

    Delete Routedemo directory

--- Routedemo/src/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import Button from 'react-bootstrap/Button';
-export default function Contact() {
-  return (
-    <>
-      <h1>This is Contact Page</h1>
-      <Button variant="primary ">Success</Button>
-    </>
-  )
-}
